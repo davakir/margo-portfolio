d1cc8463b90d437dafb06e17a23315b1379f1b5c
Darya Kiryanova <bella21abyss@gmail.com>
2016-07-30 20:45:40 +0300

    Get info about photos and set to template

--- src/AppBundle/Service/Dao/Photos.php
+++ src/AppBundle/Service/Dao/Photos.php
@@ -2,29 +2,9 @@
 
 namespace Service\Dao;
 
-use Db\PostgreSql;
+use AppBundle\Entity\Photo;
 
 class DaoPhotos
 {
-	/**
-	 * Коннект к базе
-	 * @var null
-	 */
-	protected $connect = null;
 	
-	public function __construct()
-	{
-		$this->connect = (new PostgreSql())->connect();
-	}
-	
-	public function getAlbumPhotos($albumId)
-	{
-		$query = $this->connect->prepare("
-			SELECT * FROM photos WHERE to_show = 1 AND album_id = :albumId
-		");
-		
-		$query->bindParam(':albumId', $albumId);
-		
-		$query->execute();
-	}
 }
